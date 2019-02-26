ab3f586077124737818ec11e6674ad4b96b355a7
afunTW <endlessproof@gmail.com>
2019-02-26 19:44:02 +0800

    test code: detect image (bug)

--- tools/inference_detection.py
+++ tools/inference_detection.py
@@ -1,5 +1,9 @@
 import argparse
 import logging
+import random
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
 from pathlib import Path
 from PIL import Image
 
@@ -16,23 +20,23 @@ from yolov3.utils import load_classes, non_max_suppression
 from datetime import datetime
 
 
-def detect_image(image, model, img_size=416, conf_threshold=0.8, nms_threshold=0.4):
+def detect_image(img, model, img_size=416, conf_threshold=0.8, nms_threshold=0.4):
     # resize and pad image
-    ratio = min(img_size/image.size[0], img_size/image.size[1])
-    new_w = round(image.size[0] * ratio)
-    new_h = round(image.size[1] * ratio)
-    image_transform = transforms.Compose([
-        transforms.Resize((new_h, new_w)),
+    ratio = min(img_size/img.size[0], img_size/img.size[1])
+    imw = round(img.size[0] * ratio)
+    imh = round(img.size[1] * ratio)
+    img_transforms = transforms.Compose([
+        transforms.Resize((imh, imw)),
         transforms.Pad((
-            max(int((new_h-new_w)/2), 0),
-            max(int((new_w-new_h)/2), 0)), fill=(128, 128, 128)),
-        transforms.ToTensor()        
+            max(int((imh-imw)/2),0),
+            max(int((imw-imh)/2),0)), fill=(128,128,128)),
+        transforms.ToTensor(),
     ])
 
     # convert image to Tensor
     Tensor = torch.cuda.FloatTensor
-    tensor = image_transform(image).float()
-    tensor.unsqueeze_(0)
+    tensor = img_transforms(img).float()
+    tensor = tensor.unsqueeze_(0)
     input_image = Variable(tensor.type(Tensor))
 
     # run inference on the model and get detections
@@ -100,11 +104,55 @@ def main(args: argparse.Namespace):
 
     # test
     ok, frame = cap.read()
-    frame = frame[..., ::-1]
-    frame = Image.fromarray(frame)
-    logger.debug('detect frame %d' % 1)
-    detections = detect_image(frame, model, img_size=args.img_size)
+    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    image = Image.fromarray(frame)
+    frame = np.array(image)
+    pad_x = max(frame.shape[0] - frame.shape[1], 0) * (args.img_size / max(frame.shape))
+    pad_y = max(frame.shape[1] - frame.shape[0], 0) * (args.img_size / max(frame.shape))
+    unpad_h = args.img_size - pad_y
+    unpad_w = args.img_size - pad_x
+
+    _start_time = datetime.now()
+    detections = detect_image(image, model, img_size=args.img_size)
+    _cost_time = datetime.now() - _start_time
+    logger.info(f"detect image in {_cost_time}")
     print(detections)
+
+    # draw test
+    # Get bounding-box colors
+    cmap = plt.get_cmap('tab20b')
+    bbox_palette = [cmap(i) for i in np.linspace(0, 1, 20)]
+    out_image = image.copy()
+    plt.figure()
+    fig, ax = plt.subplots(1, figsize=(12, 9))
+    ax.imshow(frame)
+
+    # draw bbox and label
+    if detections is not None:
+        unique_labels = detections[:, -1].cpu().unique()
+        num_unique_labels = len(unique_labels)
+        bbox_colors = random.sample(bbox_palette, num_unique_labels)
+
+        # browse detections and draw bbox
+        for x1, y1, x2, y2, conf, cls_conf, cls_pred in detections:
+            box_h = int(((y2 - y1) / unpad_h) * frame.shape[0])
+            box_w = int(((x2 - x1) / unpad_w) * frame.shape[1])
+            y1 = int(((y1 - pad_y // 2) / unpad_h) * frame.shape[0])
+            x1 = int(((x1 - pad_x // 2) / unpad_w) * frame.shape[1])
+
+            color = bbox_colors[int(np.where(unique_labels == int(cls_pred))[0])]
+            bbox = patches.Rectangle((x1, y1), box_w, box_h,
+                                     linewidth=2,
+                                     edgecolor=color,
+                                     facecolor='none')
+            ax.add_patch(bbox)
+            plt.text(x1, y1,
+                     s=classes[int(cls_pred)],
+                     color='white',
+                     verticalalignment='top',
+                     bbox={'color': color, 'pad': 0})
+    plt.axis('off')
+    plt.savefig('../demo/test.jpg', bbox_inches='tight', pad_inches=0.0)
 
 
 if __name__ == '__main__':
